40f9ad5b43a9eaf26b73707a66a402329b86f3e5
Joseph Woodward <joseph.woodward@xeuse.com>
2021-07-07 13:22:19 +0100

    Initial commit

---
+++ tcp/listen_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net"
+	"testing"
+)
+
+func TestListener(t *testing.T) {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer listener.Close()
+
+	t.Logf("bound to %q", listener.Addr())
+}
