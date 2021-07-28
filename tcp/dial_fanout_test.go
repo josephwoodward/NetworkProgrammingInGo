76d16700eb2597696708fee65e3a244e7d311e1d
Joseph Woodward <josephwoodward@josephs-mbp.lan>
2021-07-28 04:47:51 +0100

    Add TCP connection fanout

---
+++ tcp/dial_fanout_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"net"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestDialContextCancelFanOut(t *testing.T) {
+	// This example attempts 10 TCP connections and cancels the attempt as soon as one wins
+	ctx, cancel := context.WithDeadline(
+		context.Background(),
+		time.Now().Add(10*time.Second),
+	)
+
+	listener, err := net.Listen("tcp", "127.0.0.1:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
+
+	go func() {
+		// Only accepting a single connection
+		conn, err := listener.Accept()
+		if err == nil {
+			conn.Close()
+		}
+	}()
+
+	dial := func(ctx context.Context, address string, response chan int, id int, wg *sync.WaitGroup) {
+		defer wg.Done()
+
+		var d net.Dialer
+		conn, err := d.DialContext(ctx, "tcp", address)
+		if err != nil {
+			return
+		}
+		conn.Close()
+
+		select {
+		case <-ctx.Done():
+		case response <- id:
+		}
+	}
+
+	res := make(chan int)
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go dial(ctx, listener.Addr().String(), res, i+1, &wg)
+	}
+
+	response := <-res
+	cancel()
+	wg.Wait()
+	close(res)
+
+	if ctx.Err() != context.Canceled {
+		t.Errorf("expected canceled context; actual: %s", ctx.Err())
+	}
+
+	t.Logf("dialer %d retrived the resource", response)
+}
